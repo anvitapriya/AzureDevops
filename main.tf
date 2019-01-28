821048370f5ba33d4fb85d259928fd6a878e2785
anvitapriya <priya.girimath22@gmail.com>
2019-01-28 12:18:26 -0800

    ok

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
+provider "azurerm"
+{
+
+
+}
 
 # For creating the remote state storage
 terraform {
   backend "azure" {
-    storage_account_name = "produkscomnsada001"
-    container_name       = "prod-uks-tfstat-blob-0001"
+    storage_account_name = "anvitastorage"
+    container_name       = "anvitastorage"
     key                  = "basevms.terraform.tfstate"
     access_key           = "Na4UYXpTXBhmD6opJGFXtkrJ4GS/72MylcBdXL6WD2C/Qqaj0sJsYMygsv5rfqEh+s/ZLSe4FVabcjN48Ga5og=="
   }
@@ -11,7 +16,7 @@ terraform {
 
 # Create a resource group
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "anvitasresources" {
   name     = "${var.name}"
   location = "${var.location}"
   tags = "${var.tags}"
@@ -19,7 +24,7 @@ resource "azurerm_resource_group" "rg" {
 
 # create storage account
 
-resource "azurerm_storage_account" "main" {
+resource "azurerm_storage_account" "anvitasstorage" {
   name                     = "${var.storage_name}"
   location                 = "${var.location}"
   resource_group_name      = "${var.resource_group_name}"
@@ -30,16 +35,8 @@ resource "azurerm_storage_account" "main" {
   enable_blob_encryption   = "${var.storage_enable_blob_encryption}"
 }
 
-module "storage_container" {
-  source = "./container"
 
-  resource_group_name = "${var.resource_group_name}"
-  storage_name        = "${azurerm_storage_account.main.name}"
-  container_names     = ["${var.container_names}"]
-}
-
-
-resource "azurerm_virtual_network" "vnet" {
+resource "azurerm_virtual_network" "anvitasvnet" {
   name                = "${var.vnet_name}"
   resource_group_name = "${var.resource_group_name}"
   location            = "${var.location}"
@@ -47,93 +44,65 @@ resource "azurerm_virtual_network" "vnet" {
   dns_servers         = ["${var.vnet_dns_servers}"]
 }
 
-resource "azurerm_subnet" "snet" {
-  count                     = "${length(var.address_spaces)}"
-  name                      = "${var.name[count.index]}-${format(var.count_format, count.index + 1)}"
+resource "azurerm_subnet" "anvitassubnet" {
+  name                      = "${var.name}"
+  subnet_id                 = "${var.subnet_id}
   resource_group_name       = "${var.resource_group_name}"
   virtual_network_name      = "${var.virtual_network_name}"
-  address_prefix            = "${var.address_spaces[count.index]}"
+  address_prefix            = "${var.subnet_address_spaces}"
   network_security_group_id = "${var.network_security_group_id}"
 }
 
-resource "azurerm_network_interface" "main" {
-  count               = "${var.count}"
-  name                = "${var.nic_name}-${format(var.count_format, count.index + 1)}"
+resource "azurerm_network_interface" "anvitanic" {
+  name                = "${var.nic_name}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
 
   ip_configuration {
-    name                          = "ip-config-${format(var.count_format, count.index + 1)}"
-    subnet_id                     = "${var.vm_subnet_id}"
-    private_ip_address_allocation = "${var.vm_private_ip_address_allocation}"
-    private_ip_address            = "${var.vm_private_ip_address_allocation == "static" ? var.vm_private_ip_address : "" }"
-    #public_ip_address_id          = "${var.vm_public_ip ? format("%s", element(azurerm_public_ip.main.*.id, count.index)) : "" }"
+    name                          = "ip-config-${var.nic_name}"
+    subnet_id                     = "${var.subnet_id}"
+    private_ip_address_allocation = "${var.private_ip_address_allocation}"
+    private_ip_address            = "${var.private_ip_address}"
   }
 
   network_security_group_id = "${var.vm_network_security_group_id}"
 }
 
-resource "azurerm_virtual_machine" "main_vanilla" {
-  count                 = "${!var.use_image ? var.count : "0"}"
-  name                  = "${var.vm_name}-${format(var.count_format, count.index + 1)}"
-  location              = "${var.location}"
-  resource_group_name   = "${var.resource_group_name}"
-  vm_size               = "${var.vm_size}"
-  network_interface_ids = ["${azurerm_network_interface.main.*.id[count.index]}"]
-  availability_set_id   = "${var.availability_set_id}"
+resource "azurerm_virtual_machine" "anvita-vm" {
+  name                  = "${var.name}-vm"
+  location              = "${azurerm_resource_group.main.location}"
+  resource_group_name   = "${azurerm_resource_group.main.name}"
+  network_interface_ids = ["${azurerm_network_interface.main.id}"]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
 
   storage_image_reference {
-    publisher = "${var.vm_storage_image_reference["publisher"]}"
-    offer     = "${var.vm_storage_image_reference["offer"]}"
-    sku       = "${var.vm_storage_image_reference["sku"]}"
-    version   = "${var.vm_storage_image_reference["version"]}"
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
   }
-
   storage_os_disk {
-    name    = "${var.vm_storage_os_disk_name}-${format(var.count_format, count.index + 1)}"
-    vhd_uri = "${var.storage_blob_endpoint}${var.storage_container_os_disk_name}/${var.vm_name}-${var.vm_storage_os_disk_name}-${format(var.count_format, count.index + 1)}.vhd"
-
-    os_type       = "${var.vm_storage_os_disk_type}"
-    disk_size_gb  = "${var.vm_storage_os_disk_size}"
-    caching       = "${var.vm_storage_os_disk["caching"]}"
-    create_option = "${var.vm_storage_os_disk["create_option"]}"
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
   }
-
   os_profile {
-    computer_name  = "${var.vm_name}-${format(var.count_format, count.index + 1)}"
-    admin_username = "${var.vm_admin_username}"
-    admin_password = "${random_id.password.*.b64[count.index]}"
-    custom_data    = "${var.vm_os_profile_custom_data}"
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
   }
-
   os_profile_linux_config {
     disable_password_authentication = false
-
-    # ssh_keys {
-    #   # path = "${var.vm_ssh_key_path}"
-
-    #   path     = "/home/${var.vm_admin_username}/.ssh/authorized_keys"
-    #   key_data = "${file("${var.vm_ssh_key_name}")}"
-    # }
   }
-
-  delete_os_disk_on_termination = "${var.vm_delete_os_disk_on_termination}"
-
-  lifecycle {
-    ignore_changes = ["os_profile.admin_password"]
-  }
-
   tags {
-    Name              = "${var.vm_tags["Name"]}"
-    Terraform         = "${var.vm_tags["Terraform"]}"
-    Environment       = "${var.vm_tags["Environment"]}"
-    Location          = "${var.vm_tags["Location"]}"
-    Service           = "${var.vm_tags["Service"]}"
-    Role              = "${var.vm_tags["Role"]}"
-    Business_Unit     = "${var.vm_tags["Business_Unit"]}"
-    EA_Application_ID = "${var.vm_tags["EA_Application_ID"]}"
-    JIRA              = "${var.vm_tags["JIRA"]}"
-    ExpiryDate        = "${var.vm_tags["ExpiryDate"]}"
-    BC_Priority       = "${var.vm_tags["BC_Priority"]}"
+    environment = "staging"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,35 +1,123 @@
-# Module specific Variables
-
-# Name of the resource group
-variable name {}
-
-# Geographic location of the Resource Group
-variable location {
-  default = "eun"
+variable "module_base" {
+  default = "https://github.com/anvitapriya/AzureDevops.git"
 }
 
-# "Empty" requires fufilment , please ensure you fill in as much as possible.
-# "BC_Priority" P3 , P2 , P1 (default lowest priority is P3 )
-# "Location" Default is NorthEurope (eun)
-# "CI_build_ID" Default is empty options should be jenkins | concourse | terraform
-# Date Time Format in UTC 2018-08-01T12:00:00
-# Please see : https://confluence.devops.mnscorp.net/display/PT/Resource+Tagging 
-variable "tags" {
-  type = "map"
-  description = "Resource group tags"
+### resource-group variables
+variable "resource_group_name" {
+  default     = "Anvitasresources"
+  description = "this is to clear evaluation"
+}
 
-  default = {
-    Environment = "dev"
-    Location    = "eun"
-    Service     = "Empty"
-    Role        = "Empty"
-    CI_build_ID   = "Empty"
-    CI_build_link = "Empty"
-    Business_Unit = "Empty"
-    EA_Application_ID = "Empty"
-    Expiry_Date = "Empty"
-    BC_Priority = "P3"
-    Cost_Centre = "Empty"
-    Product_Group = "Empty"
-  }
+variable "location" {
+  default     = "Central India"
+  description = "Central India"
+}
+
+### storage variables
+
+variable "storage_account_name" {
+  default     = "anvitasstorage"
+  description = "MStorage Account name. It will keep installables, terraform states etc"
+}
+
+variable "storage_account_kind" {
+  default     = "Storage"
+  description = "Account Kind Storage."
+}
+
+variable "storage_account_tier" {
+  default     = "Storage"
+  description = "storage account t"
+}
+
+
+variable "account_replication_type"
+{
+	default = "LRS"
+	description = "default replication type"
+}
+
+variable "storage_access_tier"
+{
+   default = " "
+   description = " "
+}
+
+variable "storage_account_type" {
+  default     = "Standard_LRS"
+  description = "Storage to be created as Standard Local Redundant Storage."
+}
+
+variable "storage_enable_blob_encryption" {
+  default     = false
+  description = "Flag to enable blob encryption for common Storage account."
+}
+
+variable "container_names" {
+  default     = ["anvita-bnry-blob-0001", "anvita-tfstat-blob-0001"]
+  description = "List of Containers to be created for common storage. one for binaries and other for terraform remote states"
+}
+
+
+### virtual network variables
+
+variable "vnet_name" {
+  default     = "anvitasvnet"
+  description = "anvita virtual network"
+}
+
+variable "vnet_address_space" {
+  default     = "10.105.0.0/20"
+  description = "Address space of anvita virtual network."
+}
+
+
+variable "vnet_dns_servers" {
+  default     = "10.105.0.0/21"
+  description = "Address space of anvita virtual network."
+}
+
+
+variable "subnet_name" {
+  default     = "anvitassubnet"
+  description = "Subnet of anvita virtual network."
+}
+
+
+variable "subnet_address_spaces" {
+  default     = ["10.105.6.0/24"]
+  description = "anvita subnet address spaces."
+}
+
+variable "network_security_group_id" {
+  default     = ["10.105.6.0/24"]
+  description = "anvita swcurity group id"
+}
+
+variable "nic_name" {
+	default  = "anvitanic"
+	description = "nic name"
+}
+
+variable "subnet_id"
+{
+	default 	 =  ""
+	description  =  "subnet id"
+}
+
+variable "private_ip_address_allocation"
+{
+	default 	 =  ""
+	description  =  "private_ip_address_allocation"
+}
+
+variable "private_ip_address"
+{
+	default 	 =  ""
+	description  =  "private_ip_address"
+}
+
+variable "name"
+{
+	default  =  ""
 }
